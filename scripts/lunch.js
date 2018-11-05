f6a950b1fe6cff56d1abf127f9c78b9fdfe74ad5
System Administrator <root@COAMAC17000703.local>
2018-11-05 13:32:14 +0900

    better

--- scripts/lunch.js
+++ scripts/lunch.js
@@ -2,6 +2,18 @@
 
 module.exports = robot => {
 
+  BRAIN_KEY='notes'
+  robot.respond /add¥s+note¥s+(.+)¥s*$¥, (res) =>
+    userID=res.envelope.user.id
+    text=res.match[1]
+    notes=robot.brain.get(BRAIN_KEY)||{}
+    myNotes=notes[userID] || []
+    myNotes.push text
+    notes[userID]=myNotes
+    robot.brain.set BRAIN_KEY, notes
+    robot.brain.save()
+    res.reply "保存しました　id: '#{myNotes.length -1}'"
+
   robot.hear(/^シャッフルランチ$/, async response => {
     const timeLimit = 15; // N分
     const lowerLimit = 3;
